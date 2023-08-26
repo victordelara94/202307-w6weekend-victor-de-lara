6790fa12e134becfc3fbfa8b561fa49dad7feb59
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2023-08-26 13:37:37 +0200

    add express, morgan

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import 'dotenv/config';
 import { createServer } from 'http';
-// Import { app } from './app.js';
+import { app } from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-const server = createServer();
+const server = createServer(app);
 
 server.listen(PORT);
 
 server.on('listening', () => {
-  console.log(`Lisening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
 
 server.on('error', (error) => {
