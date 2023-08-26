c795812f4776875e12f60991dc28704f1a71c377
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2023-08-26 17:53:10 +0200

    change scripts for render

--- src/controller/anime.controller.ts
+++ src/controller/anime.controller.ts
@@ -4,51 +4,73 @@ import { Request, Response } from 'express';
 
 export class AnimeController {
   async getAll(req: Request, res: Response) {
-    const data = JSON.parse(await readFile('data.json', { encoding: 'utf-8' }));
-    res.json(data);
+    try {
+      const data = JSON.parse(
+        await readFile('data.json', { encoding: 'utf-8' })
+      );
+      res.json(data);
+    } catch (error) {
+      console.error('Error getting characters', error);
+    }
   }
 
   async getById(req: Request, res: Response) {
-    const { id } = req.params;
-    const data: any[] = JSON.parse(
-      await readFile('data.json', { encoding: 'utf-8' })
-    );
-    const item = data.find((item) => item.id === id);
-    res.json(item);
+    try {
+      const { id } = req.params;
+      const data: any[] = JSON.parse(
+        await readFile('data.json', { encoding: 'utf-8' })
+      );
+      const item = data.find((item) => item.id === id);
+      res.json(item);
+    } catch (error) {
+      console.error('Error getting characters', error);
+    }
   }
 
   async create(req: Request, res: Response) {
-    const newData = req.body;
-    newData.id = crypto.randomUUID();
-    const data: any[] = JSON.parse(
-      await readFile('data.json', { encoding: 'utf-8' })
-    );
-    data.push(newData);
+    try {
+      const newData = req.body;
+      newData.id = crypto.randomUUID();
+      const data: any[] = JSON.parse(
+        await readFile('data.json', { encoding: 'utf-8' })
+      );
+      data.push(newData);
 
-    await writeFile('data.json', JSON.stringify(data), { encoding: 'utf-8' });
+      await writeFile('data.json', JSON.stringify(data), { encoding: 'utf-8' });
 
-    res.json(newData);
+      res.json(newData);
+    } catch (error) {
+      console.error('Error getting characters', error);
+    }
   }
 
   async update(req: Request, res: Response) {
-    const { id } = req.params;
-    const data: any[] = JSON.parse(
-      await readFile('data.json', { encoding: 'utf-8' })
-    );
-    const newData = data.map((item) => (item.id === id ? req.body : item));
-    await writeFile('data.json', JSON.stringify(newData));
-    res.send(`Patch character id: ${id}`);
+    try {
+      const { id } = req.params;
+      const data: any[] = JSON.parse(
+        await readFile('data.json', { encoding: 'utf-8' })
+      );
+      const newData = data.map((item) => (item.id === id ? req.body : item));
+      await writeFile('data.json', JSON.stringify(newData));
+      res.send(`Patch character id: ${id}`);
+    } catch (error) {
+      console.error('Error getting characters', error);
+    }
   }
 
   async delete(req: Request, res: Response) {
-    const { id } = req.params;
-    const data: any[] = JSON.parse(
-      await readFile('data.json', { encoding: 'utf-8' })
-    );
-    const newData = data.filter((item) => item.id !== id);
-    await writeFile('data.json', JSON.stringify(newData), {
-      encoding: 'utf-8',
-    });
-    res.send(`Delete character id: ${id}`);
+    try {
+      const { id } = req.params;
+      const data: any[] = JSON.parse(
+        await readFile('data.json', { encoding: 'utf-8' })
+      );
+      const newData = data.filter((item) => item.id !== id);
+      await writeFile('data.json', JSON.stringify(newData), {
+        encoding: 'utf-8',
+      });
+      res.send(`Delete character id: ${id}`);
+    } catch (error) {
+      console.error('Error getting characters', error);
+    }
   }
 }
