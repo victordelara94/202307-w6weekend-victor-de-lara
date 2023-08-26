9b3aa565be243962c21a04e3c6e5305123866029
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2023-08-26 16:48:55 +0200

    Add functions for update, and delete

--- src/router/anime.router.ts
+++ src/router/anime.router.ts
@@ -1,11 +1,11 @@
 import { Router as createRouter } from 'express';
-// Import { AnimeController } from '../controller/animes.controller.js';
+import { AnimeController } from '../controller/anime.controller.js';
 
-// const animeController = new animeController();
+const animeController = new AnimeController();
 export const animeRouter = createRouter();
 
-// AnimeRouter.get('/', animeController.getAll.bind(animeController));
-// animeRouter.get('/:id', animeController.getById.bind(animeController));
-// animeRouter.post('/', animeController.create.bind(animeController));
-// animeRouter.patch('/:id', animeController.update.bind(animeController));
-// animeRouter.delete('/:id', animeController.delete.bind(animeController));
+animeRouter.get('/', animeController.getAll.bind(animeController));
+animeRouter.get('/:id', animeController.getById.bind(animeController));
+animeRouter.post('/', animeController.create.bind(animeController));
+animeRouter.patch('/:id', animeController.update.bind(animeController));
+animeRouter.delete('/:id', animeController.delete.bind(animeController));
